7aa1a9086ee3f5947be34e7b93c0f2d619912af3
Adom Oshagan <oshgana@emmanuel.edu>
2023-06-26 17:01:16 -0400

    code is working, changed "Available" to sheet name

--- finalCode.py
+++ finalCode.py
@@ -1,23 +1,21 @@
 #e2efda
 
-from openpyxl import load_workbook
-from openpyxl.styles import PatternFill
 
-# Load the existing Excel file
-file_path = "/Users/adomoshagan/Desktop/Summer 23/GitHub Portfolio/Scheduling/JYCScheduling/data.xlsx"
-workbook = load_workbook(filename=file_path)
+import openpyxl
+import re
 
-# Iterate through each sheet in the workbook
+# Load the Excel file
+workbook = openpyxl.load_workbook('/Users/adomoshagan/Desktop/Summer 23/GitHub Portfolio/Scheduling/JYCScheduling/data.xlsx')
+
+# Iterate over each sheet in the workbook
 for sheet_name in workbook.sheetnames:
     sheet = workbook[sheet_name]
-
-    # Iterate through each cell in the sheet
-    for row in sheet.iter_rows(min_row=4, max_row=29, min_col=3, max_col=9):
+    
+    # Iterate over each cell in the sheet
+    for row in sheet.iter_rows():
         for cell in row:
-            if cell.value == "Available":
-                cell.value = sheet_name
+            if cell.value and re.match(r'\bavailable\b', str(cell.value), re.IGNORECASE):
+                cell.value = sheet.title
 
-# Save the changes
-workbook.save(file_path)
-
-print("Cell text modification completed successfully.")
+# Save the modified workbook
+workbook.save('modified_file.xlsx')
